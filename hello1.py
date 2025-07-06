6d2d67b1b199df567c9339a4b24ddded52fcdcf8
Dinah Rosenbaum <brightfutures7777@gmail.com>
2025-07-06 14:28:12 +0300

    Updated hello1.py with new code

--- hello1.py
+++ hello1.py
@@ -1,1 +1,42 @@
-print('helloooo')
+#print('helloooo')
+
+# first_name = 'Dinah'
+# last_name = 'Rosenbaum'
+# full_name = first_name + ' ' + last_name
+# print(last_name)
+
+# weather = "\t It\'s \"kind of\" sunny \n hope you have a good day!"
+# print(weather)
+
+# name = 'Dinah'
+# age = 60
+# print(f'hi {name}. You are {age} years old')
+
+# selfish = '01234567'
+# print(selfish[0:8:2])
+# 0246
+
+# selfish = '01234567'
+# print(selfish[::1])
+# 01234567
+
+# selfish = '01234567'
+# print(selfish[::2])
+# 0246
+
+# selfish = '01234567'
+# print(selfish[::-1])
+# 76543210
+
+# selfish = '01234567'
+# print(selfish[::-2])
+# 7531
+
+# greet = 'hellloooo'
+# print(greet[0:len(greet)])
+# hellloooo
+
+# birth_year = input('what year were you born?')
+# age = 2025 - int(birth_year)
+# print(f'You are {age} years old')
+
