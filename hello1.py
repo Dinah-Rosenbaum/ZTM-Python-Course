be24b349d204fe1165abaf1cf7e32aee097d8c09
Dinah Rosenbaum <brightfutures7777@gmail.com>
2025-07-16 13:55:32 +0300

    Walrus Operator

--- hello1.py
+++ hello1.py
@@ -255,3 +255,14 @@
 
 
 # Walrus Operator:
+# a = 'helloooooooooo'
+# if ((n := len(a)) > 10):
+#     print(f"too long {n} elements")
+
+# while ((n := len(a)) > 1):
+#     print(n)
+#     a = a[:-1]
+# print(a)
+
+
+
